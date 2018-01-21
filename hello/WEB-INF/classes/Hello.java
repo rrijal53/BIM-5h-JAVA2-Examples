e4cb439b50be5f8eb5e6f37337ff55b363b11093
Roshan Rijal <rijalroshan94@gmail.com>
2018-01-21 08:09:42 +0545

    added servlet example

---
+++ hello/WEB-INF/classes/Hello.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Hello extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello roshan!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello rowsun!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+}
